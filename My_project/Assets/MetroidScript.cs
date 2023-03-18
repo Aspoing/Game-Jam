5d21ae63d2ca5a49b01e4b808b05972f5ad426b0
Sandro Sgro <sandro.sgro@epitech.eu>
2023-03-18 13:25:10 +0100

    [Mod] Metroid Boss has a second phase now

---
+++ My_project/Assets/MetroidScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetroidScript : MonoBehaviour
+{
+
+    public float damage = 1;
+    public float knockbackForce = 10f;
+    public float moveSpeed = 10f;
+    private float maxHealth = 100;
+    private bool isBelowMid = false;
+
+    public DetectionZone detectionZone;
+    Rigidbody2D rb;
+    DamageableCharacter damageableCharacter;
+    Animator animator;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        damageableCharacter = GetComponent<DamageableCharacter>();
+    }
+
+    void FixedUpdate() {
+        if (damageableCharacter.Health < maxHealth / 2 && !isBelowMid) {
+            animator.SetBool("isBelowMid", true);
+            isBelowMid = true;
+        }
+        if (damageableCharacter.Targetable && detectionZone.detectObjs.Count > 0) {
+            Vector2 direction = (detectionZone.detectObjs[0].transform.position - transform.position).normalized;
+            rb.AddForce(direction * moveSpeed * Time.deltaTime);
+            Debug.Log(direction * moveSpeed * Time.deltaTime);
+            // rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+            animator.SetBool("isMoving", true);
+        } else 
+            animator.SetBool("isMoving", false);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Collider2D collider = other.collider;
+        IDamageable damageable = collider.GetComponent<IDamageable>();
+
+        if (damageable != null) {
+            Vector2 direction = (Vector2) (collider.transform.position - transform.position).normalized;
+            Vector2 knockback = direction * knockbackForce;
+
+            damageable.OnHit(damage, knockback);
+        }
+    }
+}
